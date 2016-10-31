4fc92fd9924b9967fee40d2e240a9fec0fde245a
James Caldwell <james-r-caldwell@uiowa.edu>
2016-10-31 23:02:44 +0000

    made gmaps key secure

--- cartographme/app/assets/javascripts/gmaptest.js
+++ cartographme/app/assets/javascripts/gmaptest.js
@@ -4,14 +4,14 @@ RP1 = {
     handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
       markers = handler.addMarkers(gon.allMarkers);
       
-      var circles = handler.addCircles(
+      /*var circles = handler.addCircles(
         [{ lat: 41.662447, lng: -91.5311273, radius: 500 }],
         { strokeColor: '#2e633d', fillColor: '#37e869'}
-      );
+      );*/
       
       handler.bounds.extendWith(markers);
       handler.fitMapToBounds();
-      //handler.getMap().setZoom(15)
+      handler.getMap().setZoom(15)
      // handler.getMap().fitBounds(new google.maps.LatLngBounds({lat: 41.653900, lng: -91.543399},{lat: 41.670018, lng: -91.514489});
     });
     }
--- cartographme/app/controllers/home_controller.rb
+++ cartographme/app/controllers/home_controller.rb
@@ -1,16 +1,37 @@
 #Controller for main page not asscociated with any 1 model
 class HomeController < ApplicationController
   def index
+    #adds test locations to all Markers will be from locations db when done
     @allMarkers = [
       {
-        "lat" => 47.662447,
-        "lng" => -97.5311273,
-        "infowindow" => "Potential Location!"
+        "lat" => 41.659763, 
+        "lng" => -91.532282,
+        "infowindow" => "Englert Civic Theatre."
       },
       {
-        "lat" => 42.662447,
-        "lng" => -92.5311273,
-        "infowindow" => "Potential Location!"
+        "lat" => 41.661291,
+        "lng" => -91.536145,
+        "infowindow" => "Old Capitol Museum."
+      },
+      {
+        "lat" => 41.658718,
+        "lng" => -91.551107,
+        "infowindow" => "Kinnick Stadium."
+      },
+      {
+        "lat" => 41.659401,
+        "lng" => -91.526448,
+        "infowindow" => "College Green Park."
+      },
+      {
+        "lat" => 41.663622,
+        "lng" => -91.530299,
+        "infowindow" => "The Bluebird Diner."
+      },
+      {
+        "lat" => 41.666768,
+        "lng" => -91.527106,
+        "infowindow" => "North Market Park."
       }
     ]
     gon.allMarkers = @allMarkers
