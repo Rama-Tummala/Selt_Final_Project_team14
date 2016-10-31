87705cc790db851105d6ae3496aade3349aad2fa
James Caldwell <james-r-caldwell@uiowa.edu>
2016-10-31 22:43:43 +0000

    Added gon gem, gmaps4rails gem, underscore js, updated home view with map, added controller ability to add location markers to map

--- cartographme/app/controllers/home_controller.rb
+++ cartographme/app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
 #Controller for main page not asscociated with any 1 model
 class HomeController < ApplicationController
   def index
-    
+    @allMarkers = [
+      {
+        "lat" => 47.662447,
+        "lng" => -97.5311273,
+        "infowindow" => "Potential Location!"
+      },
+      {
+        "lat" => 42.662447,
+        "lng" => -92.5311273,
+        "infowindow" => "Potential Location!"
+      }
+    ]
+    gon.allMarkers = @allMarkers
   end
 end
