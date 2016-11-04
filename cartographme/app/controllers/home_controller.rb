113d1d181336ebe523ae577818e5d631a125d544
Jacob Vander Schel <jacob-vanderschel@uiowa.edu>
2016-11-04 19:49:17 +0000

    working on db/seed

--- cartographme/app/controllers/home_controller.rb
+++ cartographme/app/controllers/home_controller.rb
@@ -2,38 +2,39 @@
 class HomeController < ApplicationController
   def index
     #adds test locations to all Markers will be from locations db when done
-    @allMarkers = [
-      {
-        "lat" => 41.659763, 
-        "lng" => -91.532282,
-        "infowindow" => "Englert Civic Theatre."
-      },
-      {
-        "lat" => 41.661291,
-        "lng" => -91.536145,
-        "infowindow" => "Old Capitol Museum."
-      },
-      {
-        "lat" => 41.658718,
-        "lng" => -91.551107,
-        "infowindow" => "Kinnick Stadium."
-      },
-      {
-        "lat" => 41.659401,
-        "lng" => -91.526448,
-        "infowindow" => "College Green Park."
-      },
-      {
-        "lat" => 41.663622,
-        "lng" => -91.530299,
-        "infowindow" => "The Bluebird Diner."
-      },
-      {
-        "lat" => 41.666768,
-        "lng" => -91.527106,
-        "infowindow" => "North Market Park."
-      }
-    ]
-    gon.allMarkers = @allMarkers
+    # @allMarkers = [
+    #   {
+    #     "lat" => 41.659763, 
+    #     "lng" => -91.532282,
+    #     "infowindow" => "Englert Civic Theatre."
+    #   },
+    #   {
+    #     "lat" => 41.661291,
+    #     "lng" => -91.536145,
+    #     "infowindow" => "Old Capitol Museum."
+    #   },
+    #   {
+    #     "lat" => 41.658718,
+    #     "lng" => -91.551107,
+    #     "infowindow" => "Kinnick Stadium."
+    #   },
+    #   {
+    #     "lat" => 41.659401,
+    #     "lng" => -91.526448,
+    #     "infowindow" => "College Green Park."
+    #   },
+    #   {
+    #     "lat" => 41.663622,
+    #     "lng" => -91.530299,
+    #     "infowindow" => "The Bluebird Diner."
+    #   },
+    #   {
+    #     "lat" => 41.666768,
+    #     "lng" => -91.527106,
+    #     "infowindow" => "North Market Park."
+    #   }
+    # ]
+    # gon.allMarkers = @allMarkers
+    gon.allMarkers = @key_locations
   end
 end
--- cartographme/db/seeds.rb
+++ cartographme/db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+key_locations = Key_Location.create([
+      {
+        lat: '41.659763', 
+        lng: '-91.532282',
+        location_name: 'Englert Civic Theatre'
+      },
+      {
+        lat: '41.661291',
+        lng: '-91.536145',
+        location_name: "Old Capitol Museum."
+      },
+      {
+        lat: '41.658718',
+        lng: '-91.551107',
+        location_name: "Kinnick Stadium."
+      },
+      {
+        lat: '41.659401',
+        lng: '-91.526448',
+        location_name: "College Green Park."
+      },
+      {
+        lat: '41.663622',
+        lng: '-91.530299',
+        location_name: "The Bluebird Diner."
+      },
+      {
+        lat: '41.666768',
+        lng: '-91.527106',
+        location_name: "North Market Park."
+      }
+    ])
