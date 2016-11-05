3ed93e41088571889c7d7760afe70bc3fafb9a9b
Nhan <nhan-tran-phan@uiowa.edu>
2016-11-05 03:35:16 +0000

    deleting unecessary

--- cartographme/app/controllers/users_controller.rb
+++ cartographme/app/controllers/users_controller.rb
@@ -70,11 +70,6 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:username, :password)
-    end
     
     def profile
     end
