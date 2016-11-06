b4d397eb4414fb19bbbf4a877bcc5d5c8d8ba0a3
James Caldwell <james-r-caldwell@uiowa.edu>
2016-11-06 19:48:48 +0000

    added home controler rspec test, git ignore coverage files, added simplecov to rails helper

--- cartographme/spec/controllers/home_controller_spec.rb
+++ cartographme/spec/controllers/home_controller_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
-  describe "GET #index" do
+  describe "index places gmap marker" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
+    
+    it "assigns markers to gon" do
+      expect(Key_Location).to receive(:all).and_return([OpenStruct.new(:name =>"abc",:lat => 1,:lng => 2),OpenStruct.new(:name=>"ab", :lat =>3,:lng =>4)])
+      get :index
+      expect(assigns(:all_markers)[0]).to include("lat" => 1, "lng" => 2)
+      expect(assigns(:all_markers)[1]).to include("lat" => 3, "lng" => 4)
+    end
   end
-
+  
 end
