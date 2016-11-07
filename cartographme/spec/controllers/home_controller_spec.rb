5bdec069c07ccebac085017ebf15a13e6dce096d
James Caldwell <james-r-caldwell@uiowa.edu>
2016-11-07 04:05:11 +0000

    added key location controller rspec

--- cartographme/spec/controllers/home_controller_spec.rb
+++ cartographme/spec/controllers/home_controller_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe HomeController, type: :controller do
     
     it "assigns markers to gon" do
       fake_loc = double("loc")
-      expect(Key_Location).to receive(:all).and_return([OpenStruct.new(:name =>"abc",:lat => 1,:lng => 2),OpenStruct.new(:name=>"ab", :lat =>3,:lng =>4)])
-      allow_any_instance_of(Key_Location).to receive(:getInfoString).and_return("")
+      expect(KeyLocation).to receive(:all).and_return([OpenStruct.new(:name =>"abc",:lat => 1,:lng => 2),OpenStruct.new(:name=>"ab", :lat =>3,:lng =>4)])
+      allow_any_instance_of(KeyLocation).to receive(:getInfoString).and_return("")
       get :index
       expect(assigns(:all_markers)[0]).to include(:lat => 1, :lng => 2)
       expect(assigns(:all_markers)[1]).to include(:lat => 3, :lng => 4)
--- cartographme/spec/controllers/key_location_controller_spec.rb
+++ cartographme/spec/controllers/key_location_controller_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe KeyLocationController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+    
+    it "creates new marker" do
+      allow_any_instance_of(KeyLocation).to receive(:getInfoString).and_return("test string")
+      post :new, {"name" => "IMU" , "lat" => 200, "lng" => 100}
+      expect(assigns(:new_marker)).to include(:infowindow => "test string", :lat => "200", :lng => "100")
+    end
   end
 
 end
