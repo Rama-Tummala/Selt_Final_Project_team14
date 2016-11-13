1b0f03ab56ad51e8edf6c76d9f038c8cc909b29e
Jacob Vander Schel <jacob-vanderschel@uiowa.edu>
2016-11-13 19:31:37 +0000

    users page not working

--- cartographme/config/routes.rb
+++ cartographme/config/routes.rb
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
   post 'users/profile'
   post 'users/following'
   
-  resources :users do
-    member do
-      get :following, :followers
-    end
+  resources :users #do
+    #member do
+     # get :following, :followers
+    #end
   end
   resources :sessions
   resources :relationships, only: [:create, :destroy]
