e27a3965b60bfc015eaa0ff3e19363c988108cf4
Jacob Vander Schel <jacob-vanderschel@uiowa.edu>
2016-11-14 22:25:45 +0000

    na

--- cartographme/app/controllers/sessions_controller.rb
+++ cartographme/app/controllers/sessions_controller.rb
@@ -4,23 +4,23 @@ class SessionsController < ApplicationController
  def user_params
     params.require(:session).permit(:email, :password)
     
-  end
+ end
  
  def new
     # default: render 'new' template
-  end
+ end
  def create
-    user = User.find_by_email(params[:session][:email])
-   if user && user.authenticate(params[:session][:password])
+  user = User.find_by_email(params[:session][:email])
+  if user && user.authenticate(params[:session][:password])
   #  cookies.permanent[:session_token]= user.session_token
-  session[:session_token]=user.session_token
-  flash[:notice] = "Login successful! Welcome #{user.email}"
+   session[:session_token]=user.session_token
+   flash[:notice] = "Login successful! Welcome #{user.email}"
    redirect_to home_index_path
- else
+  else
    flash[:warning] = "Invalid email/password combination.Try again"
    render 'new'
   end
-  end 
+ end 
  
 =begin 
 def create
