d12de3f5e5d580f420ca141ec77e541b92e0ea7e
Nhan <nhan-tran-phan@uiowa.edu>
2016-11-21 01:32:44 +0000

    no real progress in rspec

--- cartographme/spec/controllers/key_location_controller_spec.rb
+++ cartographme/spec/controllers/key_location_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe KeyLocationController, type: :controller do
 
   let!(:user) { User.new(:name => "User", :email => "user@useremail.com", :password => "password", :password_confirmation => "password") }
-  let!(:keyLocation) {KeyLocation.new(:name => "Kinnick Stadium")}
+  let!(:keyLocation) {KeyLocation.new(:name => "Kinnick Stadium",:lat => "200",:lng => "100",:description=>"description",:email=>"admin")}
 
   before do
     expect(User).to receive(:find_by).and_return(user)
@@ -11,14 +11,14 @@ RSpec.describe KeyLocationController, type: :controller do
 
   describe "GET #new" do
     it "returns http success" do
-      get :new, {"name" => "IMU" , "lat" => 200, "lng" => 100, "description" => "Iowa Memorial Union, Iowa City, IA"}
+      get :new, {"name" => "IMU" , "lat" => 200, "lng" => 100, "description" => "Iowa Memorial Union, Iowa City, IA", "email"=>"admin"}
       expect(response).to have_http_status(:success)
     end
     
     it "creates new marker" do
       allow_any_instance_of(KeyLocation).to receive(:getInfoString).and_return("test string")
       post :new, {"name" => "IMU" , "lat" => 200, "lng" => 100}
-      expect(assigns(:new_marker)).to include(:infowindow => "test string", :lat => "200", :lng => "100")
+      expect(assigns(:new_marker)).to include(:description => "Iowa Memorial Union, Iowa City, IA", :lat => "200", :lng => "100")
     end
   end
   
@@ -29,9 +29,4 @@ RSpec.describe KeyLocationController, type: :controller do
     end
   end
   
-  describe "DELETE #destroy" do
-    it "destroys the key location" do
-      delete :destroy
-    end
-  end
 end
