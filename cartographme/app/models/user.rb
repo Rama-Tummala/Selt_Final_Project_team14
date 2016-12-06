f27812addee856b6afa4a333085485b6c350112e
Nhan <nhan-tran-phan@uiowa.edu>
2016-12-06 17:06:37 +0000

    a

--- cartographme/app/models/user.rb
+++ cartographme/app/models/user.rb
@@ -20,6 +20,21 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
   
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
   private
     def create_session_token
       self.session_token = User.digest(User.new_session_token)
