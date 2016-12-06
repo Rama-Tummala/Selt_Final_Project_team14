9a78b1be71ad8a5dda5731ebfd46493a2de6085e
Nhan <nhan-tran-phan@uiowa.edu>
2016-12-06 18:04:19 +0000

    numbers, card, margins

--- cartographme/app/models/user.rb
+++ cartographme/app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_and_belongs_to_many :key_locations
-  
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   before_save {|user| user.email=user.email.downcase}
   before_create :create_session_token
   validates :name, presence: true, length: {maximum: 50}
