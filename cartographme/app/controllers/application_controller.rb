1188ee7862f33cfd32110d34fc131fa25023867f
Jacob Vander Schel <jacob-vanderschel@uiowa.edu>
2016-12-07 01:03:36 +0000

    following button works, unfollow has wrong user.id somehow

--- cartographme/app/controllers/application_controller.rb
+++ cartographme/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :set_current_user
 
   protected
  # @current_user ||= User.find_by_session_token(cookies[:session_token])
@@ -17,5 +18,6 @@ class ApplicationController < ActionController::Base
     @current_user.id.to_s == id
   end  
   
+  helper_method :current_user
   
 end
--- cartographme/config/routes.rb
+++ cartographme/config/routes.rb
@@ -33,5 +33,6 @@ Rails.application.routes.draw do
   match '/signup', to: 'users#new', via: :get
   match '/login', to: 'sessions#new', via: :get
   match '/logout', to: 'sessions#destroy', via: :delete
+  
 
 end
