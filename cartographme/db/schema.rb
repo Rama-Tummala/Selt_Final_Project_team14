c6cf359bae3d79e2092b5fd2c67652543965330b
Nhan <nhan-tran-phan@uiowa.edu>
2016-12-07 21:55:18 +0000

    map works again

--- cartographme/db/schema.rb
+++ cartographme/db/schema.rb
@@ -51,15 +51,14 @@ ActiveRecord::Schema.define(version: 20161115044349) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
+    t.string   "session_token"
+    t.string   "password_digest"
     t.string   "home_city"
     t.string   "recommended_places"
     t.string   "places_visited"
     t.string   "following"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
-    t.string   "session_token"
-    t.string   "password_digest"
   end
 
 end
