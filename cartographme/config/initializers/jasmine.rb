e9999033ec91a900992d9758992d63336afa6a1d
Nhan <nhan-tran-phan@uiowa.edu>
2016-12-09 16:37:50 +0000

    last changes

--- cartographme/config/initializers/jasmine.rb
+++ cartographme/config/initializers/jasmine.rb
@@ -1,5 +1,5 @@
 if defined?(JasmineRails)
   JasmineFixtureServer = Proc.new do |env|
-    Rack::Directory.new('spec/javascripts/fixtures').call(env)
+    #Rack::Directory.new('spec/javascripts/fixtures').call(env)
   end
 end
